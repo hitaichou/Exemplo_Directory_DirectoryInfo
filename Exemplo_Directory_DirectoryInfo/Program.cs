79f94bc778c04ec53503fd800005e6c15bd6c92e
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-08-17 19:51:55 -0300

    Adicionar arquivos de projeto.

---
+++ Exemplo_Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exemplo_Directory_DirectoryInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
